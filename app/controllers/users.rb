444f262c244158c7ce51d1dc6c2bd857accc9f22
Dudisek <peta.duda@gmail.com>
2015-12-02 23:46:28 +0800

    Basic user profile, create, update, delete, show

---
+++ app/controllers/users.rb
@@ -0,0 +1,38 @@
+# DISPLAY A CREATE PROFILE FORM 
+get "/users/new" do
+	erb :"user/new" 
+end
+
+# CREATE NEW USER
+post "/users" do
+	user = User.create(name: params[:name], email: params[:email], password: params[:password])
+	redirect "/users/#{user.id}"
+end
+
+
+# VIEW PROFILE
+get "/users/:id" do
+	@user = User.find(params[:id])
+	erb :"user/show"
+end
+
+# DISPLAY USER UPDATE FORM
+get "/users/:id/edit" do
+	@user = User.find(params[:id])
+	erb :"user/edit"
+end
+
+# UPDATE PROFILE
+patch "/users/:id" do
+	user = User.find(params[:id])
+	user.update(name: params[:name], email: params[:email], password: params[:password], description: params[:description])
+	redirect "/users/#{user.id}"
+end
+
+
+# DELETE PROFILE
+delete "/users/:id" do
+	user = User.find(params[:id])
+	user.destroy
+	redirect "/"
+end
