f87dff3b1628c548464bcc1eb3a9d24af6ef5242
Dudisek <peta.duda@gmail.com>
2015-12-03 17:10:26 +0800

    User accounts with login, logout and authorization & security done

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,13 @@
-# def authorization (user)
-# 	user.id == session[user.id]
-# 		user.profile
-# 	else
-# 		som
-# end
+def authorization? (user)
+	user == session[:user_id]
+end
+
+def current_user
+	if session[:user_id]
+		current_user ||= User.find_by_id(session[:user_id])
+	end
+end
+
+def logged_in?
+	!current_user.nil?
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: /\w@\w{1}+[.]\w{2}/}, :uniqueness => true
 	validates :name, presence: true
 	validates :password, length: {minimum: 6}
+	attr_accessor :user
 
 	has_secure_password
 
 	# before_create
+
+
 end
 
