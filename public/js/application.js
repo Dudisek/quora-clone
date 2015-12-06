99e2d64786d458489ca94d07107c6d244034f1c4
Dudisek <peta.duda@gmail.com>
2015-12-06 11:28:34 +0800

    Customize User Board + Login button pop up

--- public/js/application.js
+++ public/js/application.js
@@ -11,5 +11,22 @@ $(document).ready(function(){
 	$('#myTabs a:first').tab('show') // Select first tab
 	$('#myTabs a:last').tab('show') // Select last tab
 	$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
+
+
+
+	$('#exampleModal').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget) // Button that triggered the modal
+	  var recipient = button.data('whatever') // Extract info from data-* attributes
+	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	  var modal = $(this)
+	  modal.find('.modal-title').text('Log In ')
+	  modal.find('.modal-body input').val()
+	})
+
+
 });
+
+
 
