d7f3cdeb645d2358118e2289e31d01b96b2f68f2
Dudisek <peta.duda@gmail.com>
2016-01-04 11:47:42 +0800

    fix a bit answer's votes

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -20,47 +20,47 @@ end
 
 
 
-# ANSWERS VOTES UP
-post "/questions/:question_id/answer/:answer_id/upvote" do
+# # ANSWERS VOTES UP
+# post "/answer/upvote/:id" do
 
-	voting = Avote.find_by(user_id: current_user, answer_id: params[:answer_id])
+# 	voting = Avote.find_by(user_id: current_user, answer_id: params[:answer_id])
 
-	if voting.nil?
-		a = Avote.create(upvote: 1, answer_id: params[:answer_id], user: current_user)
-		redirect "/questions/#{a.answer_id}"
-	elsif voting.upvote == 0 || voting.upvote == nil
-		voting.upvote = 1
-		voting.downvote = 0
-		voting.save
-		redirect "/questions/#{voting.answer_id}"	
-	elsif voting.upvote == 1
-		voting.upvote = 0
-		voting.save
-		redirect "/questions/#{voting.answer_id}"	
-	end
-end
+# 	if voting.nil?
+# 		a = Avote.create(upvote: 1, answer_id: params[:answer_id], user: current_user)
+# 		redirect "/questions/#{a.answer_id}"
+# 	elsif voting.upvote == 0 || voting.upvote == nil
+# 		voting.upvote = 1
+# 		voting.downvote = 0
+# 		voting.save
+# 		redirect "/questions/#{voting.answer_id}"	
+# 	elsif voting.upvote == 1
+# 		voting.upvote = 0
+# 		voting.save
+# 		redirect "/questions/#{voting.answer_id}"	
+# 	end
+# end
 
-# ANSWERS VOTES DOWN
+# # ANSWERS VOTES DOWN
 
-post "/questions/:question_id/answer/:answer_id/downvote" do
+# post "/answer/downvote/:id" do
 
-	voting = Avote.find_by(user_id: current_user, answer_id: params[:answer_id])
+# 	voting = Avote.find_by(user_id: current_user, answer_id: params[:answer_id])
 
-	if voting.nil?
-		a = Avote.create(downvote: 1, answer_id: params[:answer_id], user: current_user)
-		redirect "/questions/#{a.answer_id}"
+# 	if voting.nil?
+# 		a = Avote.create(downvote: 1, answer_id: params[:answer_id], user: current_user)
+# 		redirect "/questions/#{a.answer_id}"
 
-	elsif voting.downvote == 0 || voting.downvote == nil
-		voting.downvote = 1
-		voting.upvote = 0
-		voting.save
-		redirect "/questions/#{voting.answer_id}"	
-	elsif voting.downvote == 1 
-		voting.downvote = 0
-		voting.save
-		redirect "/questions/#{voting.answer_id}"	
-	else
-		puts "\e[31m[LOG: ] VOTING ERROR UNEXPECTED SITUATION"
-		erb :"404"
-	end
-end
+# 	elsif voting.downvote == 0 || voting.downvote == nil
+# 		voting.downvote = 1
+# 		voting.upvote = 0
+# 		voting.save
+# 		redirect "/questions/#{voting.answer_id}"	
+# 	elsif voting.downvote == 1 
+# 		voting.downvote = 0
+# 		voting.save
+# 		redirect "/questions/#{voting.answer_id}"	
+# 	else
+# 		puts "\e[31m[LOG: ] VOTING ERROR UNEXPECTED SITUATION"
+# 		erb :"404"
+# 	end
+# end
